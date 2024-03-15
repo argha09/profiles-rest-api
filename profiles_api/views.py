9a5cfef75fb0d6f6f8bd50567af6b1895ceee1c7
Argha <argha.saha.official@gmail.com>
2024-03-15 14:45:20 +0530

    Hello API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    ''' Test API View '''
+
+    def get(self, request, format=None):
+        ''' Returns a list of APIView features '''
+        an_apiview = [
+            'Arindam Saha',
+            'Anirban Mukherjee',
+            'Argha Saha',
+        ]
+
+        return Response({'message': 'Hello!', 'ap_apiview': an_apiview})
